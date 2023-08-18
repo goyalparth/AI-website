df9f466b251c403c96feee598ce30e57d239934e
Parth <goyalparth97@gmail.com>
2023-08-18 23:45:25 +1000

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using AspNetCoreMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AspNetCoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("Jobs")]
+        public IActionResult Jobs()
+        {
+            return View();
+        }
+
+        [Route("Companies")]
+        public IActionResult Companies()
+        {
+            return View();
+        }
+
+        [Route("Contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,206 @@
+﻿var technologies = [{
+        "title": " Machine Learning",
+        "image": "../images/technologies/ml.jpg",
+        "description": "Machine learning (ML) is a field of inquiry devoted to understanding and building methods that 'learn', that is, methods that leverage data to improve performance on some set of tasks.It is seen as a part of artificial intelligence (Wikipedia).",
+        "url": "https://en.wikipedia.org/wiki/Machine_learning"
+    },
+    {
+        "title": "Deep Learning",
+        "image": "../images/technologies/dl.jpg",
+        "description": "Deep learning (also known as deep structured learning) is part of a broader family of machine learning methods based on artificial neural networks with representation learning.Learning can be supervised, semi- supervised or unsupervised (Wikipedia).",
+        "url": "https://en.wikipedia.org/wiki/Deep_learning"
+    },
+    {
+        "title": "Natural Language Processing (NLP)",
+        "image": "../images/technologies/nlp.jpg",
+        "description": "Natural language processing (NLP) is a subfield of linguistics, computer science, and AI concerned with the interactions between computers and human language, in particular how to program computers to process and analyze large amounts of natural language data (Wikipedia).",
+        "url": "https://en.wikipedia.org/wiki/Natural_language_processing"
+    },
+    {
+        "title": "Neural Network",
+        "image": "../images/technologies/nn.jpg",
+        "description": "A neural network is a network or circuit of biological neurons, or, in a modern sense, an artificial neural network, composed of artificial neurons or nodes.Thus, a neural network is either a biological neural network, made up of biological neurons, or an artificial neural network, used for solving artificial intelligence(AI) problems (Wikipedia).",
+        "url": "https://en.wikipedia.org/wiki/Neural_network"
+    },
+    {
+        "title": "Cognitive Computing",
+        "image": "../images/technologies/cc.jpg",
+        "description": "Cognitive computing (CC) refers to technology platforms that, broadly speaking, are based on the scientific disciplines of AI and signal processing.These platforms encompass ML, reasoning, NLP, speech recognition and vision recognition, humancomputer interaction, dialog and narrative generation, among other technologies (Wikipedia).",
+        "url": "https://en.wikipedia.org/wiki/Cognitive_computing"
+    },
+    {
+        "title": "Computer Vision",
+        "image": "../images/technologies/cv.jpg",
+        "description": "Computer vision is an interdisciplinary scientific field that deals with how computers can gain high- level understanding from digital images or videos.From the perspective of engineering, it seeks to understand and automate tasks that the human visual system can do (Wikipedia).",
+        "url": "https://en.wikipedia.org/wiki/Computer_vision"
+    }];
+
+var companies = [
+    {
+        "title": "Alphabet (Google)",
+        "image": "../images/companies/alphabet.jpg",
+        "description": "Google was reorganized as a wholly owned subsidiary of Alphabet Inc. Google is Alphabet's largest subsidiary and is a holding company for Alphabet's Internet properties and interests. Sundar Pichai was appointed CEO of Google on October 24, 2015, replacing Larry Page, who became the CEO of Alphabet. On December 3, 2019, Pichai also became the CEO of Alphabet.<br /> On August 10, 2015, Google announced plans to reorganize its various interests as a conglomerate named Alphabet Inc. Google became Alphabet's largest subsidiary and the umbrella company for Alphabet's Internet interests. Upon completion of the restructuring, Sundar Pichai became CEO of Google, replacing Larry Page, who became CEO of Alphabet. (Wikipedia)",
+        "bookmark": "google"
+    },
+    {
+        "title": "Amazon",
+        "image": "../images/companies/amazon.jpg",
+        "description": "Amazon.com, Inc. is an American multinational technology company which focuses on ecommerce, cloud computing, digital streaming, and artificial intelligence. It has been referred to as \"one of the most influential economic and cultural forces in the world\", and is one of the world's most valuable brands. It is one of the Big Five American information technology companies, alongside Alphabet, Apple, Meta, and Microsoft. <br> Amazon was founded by Jeff Bezos from his garage in Bellevue, Washington, on July 5, 1994.Initially an online marketplace for books, it has expanded into a multitude of product categories: a strategy that has earned it the moniker The Everything Store.It has multiple subsidiaries including Amazon Web Services(cloud computing), Zoox(autonomous vehicles), Kuiper Systems(satellite Internet), and Amazon Lab126(computer hardware R&D). (Wikipedia)",
+        "bookmark": "amazon"
+    },
+    {
+        "title": "Apple",
+        "image": "../images/companies/apple.jpg",
+        "description": "Apple Inc. is an American multinational technology company that specializes in consumer electronics, software and online services headquartered in Cupertino, California, United States.Apple is the largest technology company by revenue(totaling US$365.8 billion in 2021) and as of June 2022, it is the world's biggest company by market capitalization, the fourth- largest personal computer vendor by unit sales and secondlargest mobile phone manufacturer.It is one of the Big Five American information technology companies, alongside Alphabet, Amazon, Meta, and Microsoft.<br> Apple was founded as Apple Computer Company on April 1, 1976, by Steve Jobs, Steve Wozniak and Ronald Wayne to develop and sell Wozniak's Apple I personal computer. It was incorporated by Jobs and Wozniak as Apple Computer, Inc.in 1977 and the company's next computer, the Apple II, became a best seller. (Wikipedia)",
+        "bookmark": "apple"
+    },
+    {
+        "title": "C3.AI",
+        "image": "../images/companies/c3ai.jpg",
+        "description": "C3.ai is an enterprise AI company that focuses on delivering machine learning software to industry leaders.Some of the company's customers include Shell PLC (SHEL), the U.S. Air Force and Enel SpA(ENLAY).Its software is flexible, providing financial security, product reliability and secure systems across its customer base.For example, by partnering with utility companies like Enel, Duke Energy Corp. (DUK) and the New York Power Authority, C3 assists with ensuring that grid capacity is maximized across the U.S.AI stock is down 36% this year as of July 8, but given its deep foothold across both corporate and governmental end users, it's a company to keep an eye out for. (https://money.usnews.com/investing/stock-market-news/slideshows/artificial-intelligencestocks-the-10-best-ai-companies?slide=7)",
+        "bookmark": "c3ai"
+    },
+    {
+        "title": "DocuSign",
+        "image": "../images/companies/docusign.jpg",
+        "description": "You might not consider online document signing as the next hub for AI-fueled innovation, but DOCU is making advancements.By providing companies with machine learning tools, DOCU make finding clauses in contracts and signing documents easier and faster.Contract AI can also help businesses understand the legal requirements of documents, locate potential issues that could arise from signing and then help organize contracts with a click of a button.DOCU is down about 56% this year through July 8, but analysts expect double- digit revenue growth in fiscal 2023 and fiscal 2024. (https://money.usnews.com/investing/stock-market-news/slideshows/artificial-intelligencestocks-the-10-best-ai-companies?slide=14)",
+        "bookmark": "docusign"
+    },
+    {
+        "title": "IBM",
+        "image": "../images/companies/ibm.jpg",
+        "description": "One of the longest-running computing companies, IBM has been at the forefront of data storage and computing technologies for decades.The company’s hallmark AI technology, Watson, aimed to revolutionize how businesses use AI as part of everyday operations. Seventy percent of global banking institutions use the software to streamline their services and provide secure financial transactions.The software can even work in tandem with Azure and AWS, meaning that businesses can use Watson without fully transitioning the framework of their operations.The company saw 8% revenue growth last quarter, with gains led by software and consulting services, which had increases of 12% and 13%, respectively.The company anticipates revenue to continue increasing at a similar pace for the rest of its fiscal year, at the high end of the mid- single digits. (https://money.usnews.com/investing/stock-market-news/slideshows/artificial-intelligencestocks-the-10-best-ai-companies?slide=13)",
+        "bookmark": "ibm"
+    },
+    {
+        "title": "Meta Platforms",
+        "image": "../images/companies/metaplatforms.jpg",
+        "description": "Meta changed its image and name as part of its expansion into AI technology. Machine learning was always a keystone of its social media platform Facebook, as algorithms worked to connect individuals with content and companies that would be of interest to them.Now, Meta is letting its AI capabilities seep into different parts of its business.The prospects of this are massive, as recently was shown when AI tools at Meta enabled the translation of over 200 languages with an average increased quality of 44%, when compared to previous AI research.By broadening the range of how its programs use AI, such breakthroughs are bound to become more common.Meta stock is down almost 50% this year through July 8, placing it in possible value stock territory.Meta's current forward price - earnings ratio is 14.3. (https://money.usnews.com/investing/stock-marketnews/slideshows/artificial-intelligence-stocks-the-10-best-ai-companies?slide=10)",
+        "bookmark": "meta"
+    },
+    {
+        "title": "Micron Technology",
+        "image": "../images/companies/micron.jpg",
+        "description": "Micron is a computer and storage chip manufacturer that produces memory and storage technology for a broad range of industries.The company recently began producing more chips in Japan and announced a $150 billion global expansion plan.The plan will boost investment in manufacturing, research and development by partnering with governments across the world to diversify Micron's capacity to deliver products. While the company recently warned of declining demand that will affect its products due to the rise in inflation, global demand for advanced chips is bound to increase over the long term as the range of products using those chips also increases.However, Sawhney notes that the current conditions paint a rocky picture of its short- term prospects, stating that \"although its third - quarter revenue jumped 16 % to $8.64 billion, measuring up to market estimates, MU expects its fourth - quarter revenue to plunge 13 % year over year.\" (https://money.usnews.com/investing/stock-market-news/slideshows/artificial-intelligencestocks-the-10-best-ai-companies?slide=6)",
+        "bookmark": "micron"
+    },
+    {
+        "title": "Microsoft",
+        "image": "../images/companies/microsoft.jpg",
+        "description": "Microsoft began as a software company but now reaches deep into the AI sphere with its cloud computing program, Azure.Offering secure storage and data capacity to corporations, Azure is a global leader in cloud computing and software development. Recently, Microsoft detailed how its facial recognition software could be used in an effort to increase ethical use of AI.Such moves will prove pivotal as legislators and consumers begin to worry about the privacy threats that AI can pose.Putting these ethical concerns first ensures that Azure is a step ahead to bypass such concerns.Azure and other cloud computing revenue grew 46% year over year last quarter. (https://money.usnews.com/investing/stock-market-news/slideshows/artificial-intelligencestocks-the-10-best-ai-companies?slide=11)",
+        "bookmark": "microsoft"
+    },
+    {
+        "title": "Nvidia",
+        "image": "../images/companies/nvidia.jpg",
+        "description": "Nvidia Corporation, commonly known as Nvidia, is an American multinational technology company incorporated in Delaware and based in Santa Clara, California.It is a software and fabless company which designs graphics processing units(GPUs), application programming interface(APIs) for data science and high- performance computing as well as system on a chip units(SoCs) for the mobile computing and automotive market.Nvidia is a global leader in artificial intelligence hardware and software.Its professional line of GPUs are used in workstations for applications in such fields as architecture, engineering and construction, media and entertainment, automotive, scientific research, and manufacturing design.<br> In addition to GPU manufacturing, Nvidia provides an API called CUDA that allows the creation of massively parallel programs which utilize GPUs.They are deployed in supercomputing sites around the world.More recently, it has moved into the mobile computing market, where it produces Tegra mobile processors for smartphones and tablets as well as vehicle navigation and entertainment systems.In addition to AMD, its competitors include Intel, Qualcomm and AI - accelerator companies such as Graphcore. (Wikipedia)",
+        "bookmark": "nvidia"
+    },
+    {
+        "title": "Oracle",
+        "image": "../images/companies/oracle.jpg",
+        "description": "Oracle Corporation is an American multinational computer technology corporation headquartered in Austin, Texas.In 2020, Oracle was the third- largest software company in the world by revenue and market capitalization.[6] The company sells database software and technology(particularly its own brands), cloud engineered systems, and enterprise software products, such as enterprise resource planning(ERP) software, human capital management(HCM) software, customer relationship management(CRM) software(also known as customer experience), enterprise performance management(EPM) software, and supply chain management(SCM) software. <br> Larry Ellison co - founded Oracle Corporation in 1977 with Bob Miner and Ed Oates under the name Software Development Laboratories(SDL).[2] Ellison took inspiration[8] from the 1970 paper written by Edgar F. Codd on relational database management systems(RDBMS) named 'A Relational Model of Data for Large Shared Data Banks.' He heard about the IBM System R database from an article in the IBM Research Journal provided by Oates.Ellison wanted to make Oracle's product compatible with System R, but failed to do so as IBM kept the error codes for their DBMS a secret.",
+        "bookmark": "oracle"
+    },
+    {
+        "title": "Siemens",
+        "image": "../images/companies/siemens.jpg",
+        "description": "Siemens AG (German pronunciation: [ˈziːməns] (listen) or [-mɛns]) is a German multinational conglomerate corporation and the largest industrial manufacturing company in Europe headquartered in Munich with branch offices abroad.<br> The principal divisions of the corporation are Industry, Energy, Healthcare(Siemens Healthineers), and Infrastructure & Cities, which represent the main activities of the corporation.The corporation is a prominent maker of medical diagnostics equipment and its medical healthcare division, which generates about 12 percent of the corporation's total sales, is its second - most profitable unit, after the industrial automation division.The corporation is a component of the Euro Stoxx 50 stock market index.[10] Siemens and its subsidiaries employ approximately 303, 000 people worldwide and reported global revenue of around €62 billion in 2021 according to its earnings release. (Wikipedia)",
+        "bookmark": "siemens"
+    },
+];
+
+$(document).ready(function () {
+    if (location.pathname == '/') {
+        //Add 6 Technologies on the home page.
+        var homeTechnologiesContent = $(`<div class="row m-0 g-0"></div>`);
+        for (i = 0; i < technologies.length; i++) {
+            var technology = technologies[i];
+            homeTechnologiesContent.append(`
+            <figure class="home-technology col-md-4 col-sm-6 col-12">
+                <img src="${technology.image}">
+                <figcaption class="p-4">
+                    <h2>${technology.title}</h2>
+                    <p>${technology.description}</p>
+                    <a href="${technology.url}">Read more</a>
+                </figcaption>
+            </figure>
+        `);
+        }
+        $('#AI-tech').append(homeTechnologiesContent);
+
+        //Add 12 Companies on the home page
+        var homeCompaniesContent = $(`<div class="row m-0 p-4 g-4"></div>`);
+        for (i = 0; i < companies.length; i++) {
+            var company = companies[i];
+            homeCompaniesContent.append(`
+            <div class="col-lg-2 col-md-3 col-sm-4 col-6">
+                <figure class="home-company" data-bookmark="#${company.bookmark}">
+                    <img src="${company.image}">
+                    <figcaption>
+                        <h2>${company.title}</h2>
+                    </figcaption>
+                </figure>
+            </div>
+        `);
+        }
+        $('#AI-company').append(homeCompaniesContent);
+    }
+
+
+    if (location.pathname == '/Companies') {
+        //Add 12 Companies on the companies page
+        for (i = 0; i < companies.length; i++) {
+            var company = companies[i];
+            if (i % 2 == 0) {
+                $('#companies-list').append(`<div id="${company.bookmark}" class="company row m-0 g-0">
+                    <div class="company-image col-md-6 col-12">
+                        <img class="img-fluid" src="${company.image}">
+                    </div>
+                    <div class="company-description col-md-6 col-12">
+                        <h2>${company.title}</h2>
+                        <p>${company.description}</p>
+                    </div>
+                </div>
+            `);
+            }
+            else {
+                $('#companies-list').append(`<div id="${company.bookmark}" class="company row m-0 g-0">
+                    <div class="company-description col-md-6 col-12">
+                        <h2>${company.title}</h2>
+                        <p>${company.description}</p>
+                    </div>
+                    <div class="company-image col-md-6 col-12">
+                        <img class="img-fluid" src="${company.image}">
+                    </div>
+                </div>
+            `);
+            }
+        }
+
+        if (location.hash) {
+            console.log('test');
+            var target = location.hash;
+            $('html,body').animate({
+                scrollTop: $(target).offset().top
+            }, 100);
+        }
+    }
+
+    $('figure').attr('style', 'padding:0px; margin:0px;');
+});
+
+
+//Additional Events
+$(document).on('click', '.home-company', function () {
+    var bookmark = $(this).data('bookmark');
+    location.href = "/Companies" + bookmark;
+});
+
+$(document).on('mouseover', '#home-jobs h1', function () {
+    $('#home-jobs img').fadeIn(2000);
+});
+
+$(document).on('click', '#home-jobs img', function () {
+    location.href = "/Jobs";
+});
